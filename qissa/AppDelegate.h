9d3e5058381de1f5bf760e50470f8af566e731c1
Qusay <qissa.uba@gmail.com>
2016-02-03 11:09:37 +0200

    Initial Commit

---
+++ qissa/AppDelegate.h
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.h
+//  qissa
+//
+//  Created by Qusay Bany Issa on 2/3/16.
+//  Copyright © 2016 Qusay Bany Issa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+
+@end
+
---
+++ qissa/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  qissa
+//
+//  Created by Qusay Bany Issa on 2/3/16.
+//  Copyright © 2016 Qusay Bany Issa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
